d05a4792cdc536705046cd0e249d1ab1cfa812f3
Jon Parrish <jonparrish@rocketmail.com>
2012-03-11 21:32:04 -0500

    modified searchbar and searchpage

--- wiki/views.py
+++ wiki/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist
 
 class SearchForm(forms.Form):
 	text = forms.CharField(label="")
@@ -11,17 +12,24 @@ class SearchForm(forms.Form):
 def search_page(request):
 	if request.method == "POST":
 		f = SearchForm(request.POST)
+		sb = SearchForm()
 		if not f.is_valid():
 			return render_to_response("search.html", {"form":f}, RequestContext(request))
 		else:
-			pages = Page.objects.filter(name__contains = f.cleaned_data["text"])
-			contents = []
-			if f.cleaned_data["search_content"]:
-				contents = Page.objects.filter(content__contains = f.cleaned_data["text"])
-			return render_to_response("search.html", {"form":f, "pages": pages, "contents": contents}, RequestContext(request))
+			try:
+				pages = Page.objects.filter(name__contains = f.cleaned_data["text"])
+				contents = []
+				if f.cleaned_data["search_content"]:
+					contents = Page.objects.filter(content__contains = f.cleaned_data["text"])
+				return render_to_response("search.html", {"form":f, "pages": pages, "contents": contents, "sb": sb}, RequestContext(request))
+			except ObjectDoesNotExist:
+				f = SearchForm()
+				show_searchbox = True
+				return render_to_response("search.html", {"show_searchbox": show_searchbox, "form": f}, RequestContext(request))
 			
 	f = SearchForm()
-	return render_to_response("search.html", {"form":f}, RequestContext(request))
+	show_searchbox = True
+	return render_to_response("search.html", {"show_searchbox": show_searchbox, "form":f}, RequestContext(request))
 
 specialPages = {"SearchPage": search_page}
 
@@ -35,7 +43,7 @@ def view_page(request, page_name):
 			contents = []
 			if f.cleaned_data["search_content"]:
 				contents = Page.objects.filter(content__contains = f.cleaned_data["text"])
-			return render_to_response("search.html", {"form":f, "pages":pages, "contents":contents}, RequestContext(request))
+			return render_to_response("search.html", {"form": f, "pages":pages, "contents":contents}, RequestContext(request))
 	if page_name in specialPages:
 		return specialPages[page_name](request)
 	f = SearchForm()
